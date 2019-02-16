f52cca92149bde12b4797d483de364bc75817b8a
Luiz Américo <luizamericop@gmail.com>
2019-02-16 20:25:02 -0300

    Use valueOf when converting String object values

---
+++ tests/unit/object.spec.js
@@ -0,0 +1,23 @@
+const PDFObject = require('../../lib/object').default;
+
+describe('PDFObject', () => {
+  describe('convert', () => {
+    test('string literal', () => {
+      expect(PDFObject.convert('test')).toEqual('/test');
+    });
+
+    test('string literal with unicode', () => {
+      expect(PDFObject.convert('αβγδ')).toEqual('/αβγδ');
+    });
+
+    test('String object', () => {
+      expect(PDFObject.convert(new String('test'))).toEqual('(test)');
+    });
+
+    test('String object with unicode', () => {
+      const result = PDFObject.convert(new String('αβγδ'));
+      expect(result.length).toEqual(12);
+      expect(result).toMatchInlineSnapshot(`"(þÿ±²³´)"`);
+    });
+  });
+});
