6478048630d128c38261e34257a6c192816a9545
Jim Pravetz <jpravetz@gmail.com>
2019-07-21 18:31:13 -0700

    Bug fixes for setting Ff flag and alignment.

--- demo/test-acroform.js
+++ demo/test-acroform.js
@@ -19,14 +19,14 @@ doc.initAcroForm();
 let rootField = doc.field('rootField');
 let child1Field = doc.field('child1Field', { Parent: rootField });
 let child2Field = doc.field('child2Field', { Parent: rootField });
-doc.formText('leaf1', 10, 10, 200, 40, { Parent: child1Field })
-doc.formText('leaf2', 10, 60, 200, 40, { Parent: child1Field })
-doc.formText('leaf3', 10, 110, 200, 40, { Parent: child2Field })
+doc.formText('leaf1', 10, 10, 200, 40, { Parent: child1Field, multiline: true })
+doc.formText('leaf2', 10, 60, 200, 40, { Parent: child1Field, multiline: true })
+doc.formText('leaf3', 10, 110, 200, 80, { Parent: child2Field, multiline: true })
 
 var opts = {
   backgroundColor: 'yellow',
   label: 'Test Button',
 };
-doc.formPushButton('btn1', 10, 160, 100, 30, opts);
+doc.formPushButton('btn1', 10, 200, 100, 30, opts);
 
 doc.end();
