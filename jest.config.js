56978221e9dd43ffc79fa8b4aa1719c1ff300ed3
Jim Pravetz <jpravetz@gmail.com>
2019-07-29 17:33:12 -0700

    Reverting jest.config.js changes from this PR

--- jest.config.js
+++ jest.config.js
@@ -124,7 +124,6 @@ module.exports = {
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
   // setupFilesAfterEnv: [],
-  setupFilesAfterEnv: ['<rootDir>/tests/unit/setupTests.js'],
 
   // A list of paths to snapshot serializer modules Jest should use for snapshot testing
   // snapshotSerializers: [],
