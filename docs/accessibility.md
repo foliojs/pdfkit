bc09c32d1b421da03041472aadb62220783b61ca
Ben Schmidt <insightfuls@users.noreply.github.com>
2020-09-28 04:49:09 +1000

    Support marked structure content and logical structure.

--- docs/accessibility.md
+++ docs/accessibility.md
@@ -1,7 +1,11 @@
 # Accessibility
 
-Marked content sequences are foundational to creating accessible PDFs, which are usable by
-visually impaired users who rely on screen readers/text-to-speech engines/vocalisation.
+Accessible PDFs are usable by visually impaired users who rely on
+screen readers/text-to-speech engines/vocalisation.
+
+## Marked Content
+
+Marked content sequences are foundational to creating accessible PDFs.
 
 All marked content sequences are associated with a registered tag, such as 'Span'.
 
@@ -14,3 +18,118 @@ Example of marking content:
 
 Marked content is automatically ended when a page is ended, and if a new page is automatically
 added by text wrapping, marking is automatically begun again on the new page.
+
+## Logical Structure
+
+Logical structures defines the reading order of a document, and can provide alternative text
+for images and other visual content.
+
+To define logical structure, you need to mark the structure content, keep a reference to it,
+then incorporate it into a structure tree.
+
+> So far, PDFKit only supports marked content in the logical structure, not annotations, forms,
+> or anything else.
+
+Example of marking structure content:
+
+    // Mark some text as a paragraph ("P"); the tag should match the intended structure element's type
+    const myStructContent = doc.markStructureContent('P');
+    doc.text('Hello, world!');
+    doc.endMarkedContent();
+
+Example of the simplest of structure trees:
+
+    // Add a single structure element which includes the structure content to the document's structure
+    doc.addStructure(doc.struct('P', [ myStructContent ]));
+
+### Automatic Ending of Structure Content
+
+Structure content does not nest; marking structure content will automatically end current
+marking of structure content (and any descendent marking):
+
+    // Mark multiple paragraphs without needing to close them
+    const myStructContent = doc.markStructureContent('P');
+    doc.text('Hello, world!');
+    const myStructContent = doc.markStructureContent('P');
+    doc.markContent('Span');
+    doc.text('Bonjour, tout le monde!');
+    const myStructContent = doc.markStructureContent('P');
+    doc.text('Hello again!');
+
+### Complex Structure
+
+Multiple elements may be added directly to the document, and may nest:
+
+    // Create nested structure elements
+    const section1 = doc.struct('Section', [
+        doc.struct('P', [
+            someTextStructureContent,
+            doc.struct('Link', [ someLinkStructureContent ]),
+            moreTextStructureContent
+        ])
+    ]);
+    const section2 = doc.struct('Section', [ secondSectionStructureContent ]);
+
+    // Add them to the document's structure
+    doc.addStructure(section1).addStructure(section2);
+
+### Incremental Construction of Structure
+
+Structure can be built incrementally. Elements can optionally be (recursively) ended once
+you have finished adding to them, allowing them to be flushed out as soon as possible:
+
+    // Begin a new section and add it to the document's structure
+    const mySection = doc.struct('Section');
+    doc.addToStructure(mySection);
+
+    // Create a new paragraph and add it to the section
+    const myParagraph = doc.struct('P');
+    mySection.add(myParagraph);
+
+    // Add content, both to the page, and the paragraph
+    const myParagraphContent = doc.markStructureContent('P');
+    myParagraph.add(myParagraphContent);
+    doc.text('Hello, world!');
+
+    // End the paragraph, allowing it to be flushed out, freeing memory
+    myParagraph.end();
+
+Note that if you provide content when creating a structure element (i.e. providing it to
+`doc.struct()` rather than using `structElem.add()`) then `structElem.end()` is called
+automatically. You therefore should not add additional content, as the element may already have
+been flushed out. Do not mix atomic and incremental styles for the same structure element.
+
+### Structure Element Options
+
+When creating a structure element, you can provide options:
+
+ * `title` - title of the structure element (e.g. "Chapter 1")
+ * `lang` - human language code (e.g. `en-AU`) which overrides default document language
+ * `alt` - alternative text for an image or other visual content
+ * `expanded` - the expanded form of an abbreviation or acronym
+ * `actual` - the actual text the content represents (e.g. if it is rendered as vector graphics)
+
+Example of a structure tree with options specified:
+
+    const titlePage = doc.struct('Section', {
+        title: 'Title Page'
+    }, [
+        doc.struct('H', [
+            doc.struct('Span', {
+                expanded: 'Portable Document Format/Universal Accessibility',
+                actual: 'PDF/UA'
+            }, [
+                pdfUAStructureContent
+            ]),
+            doc.struct('Span', {
+                actual: 'in a Nutshell'
+            }, [
+                inANutshellStructureContent
+            ]),
+        ]),
+        doc.struct('Figure', {
+            alt: 'photo of a concrete path with tactile paving'
+        }, [
+            photoStructureContent
+        ])
+    ]);
--- tests/unit/markings.spec.js
+++ tests/unit/markings.spec.js
@@ -12,7 +12,7 @@ describe('Markings', () => {
   });
 
   describe('marked content', () => {
-    test('with no dictionary', () => {
+    test('non-structural', () => {
       const docData = logData(document);
 
       const stream = Buffer.from(
@@ -39,22 +39,35 @@ EMC
       ]);
     });
 
-    test('with empty dictionary', () => {
+    test('structural', () => {
       const docData = logData(document);
 
       const stream = Buffer.from(
         `1 0 0 -1 0 792 cm
 /Span <<
+/MCID 0
+>> BDC
+EMC
+/Span <<
+/MCID 1
 >> BDC
 EMC
 `,
         'binary'
       );
 
-      document.markContent("Span", {});
+      const structureContent1 = document.markStructureContent("Span");
+      document.endMarkedContent();
+      const structureContent2 = document.markStructureContent("Span");
       document.endMarkedContent();
       document.end();
 
+      expect(structureContent1.refs.length).toEqual(1);
+      expect(structureContent1.refs[0].pageRef.toString()).toEqual('7 0 R');
+      expect(structureContent1.refs[0].mcid).toEqual(0);
+      expect(structureContent2.refs.length).toEqual(1);
+      expect(structureContent2.refs[0].pageRef.toString()).toEqual('7 0 R');
+      expect(structureContent2.refs[0].mcid).toEqual(1);
       expect(docData).toContainChunk([
         `5 0 obj`,
         `<<
@@ -73,12 +86,25 @@ EMC
       const stream = Buffer.from(
         `1 0 0 -1 0 792 cm
 /Span BMC
+/P <<
+/MCID 0
+>> BDC
+/Span BMC
+EMC
+EMC
+/P <<
+/MCID 1
+>> BDC
+EMC
 EMC
 `,
         'binary'
       );
 
       document.markContent("Span");
+      document.markStructureContent("P");
+      document.markContent("Span");
+      document.markStructureContent("P");
       document.end();
 
       expect(docData).toContainChunk([
@@ -98,19 +124,29 @@ EMC
 
       const stream = Buffer.from(
         `1 0 0 -1 0 792 cm
+/P <<
+/MCID 0
+>> BDC
 /Span BMC
+EMC
 EMC
 `,
         'binary'
       );
 
+      const structureContent = document.markStructureContent("P");
       document.markContent("Span");
       document.text("on the first page");
       document.continueOnNewPage();
       document.end();
 
+      expect(structureContent.refs.length).toEqual(2);
+      expect(structureContent.refs[0].pageRef.toString()).toEqual('7 0 R');
+      expect(structureContent.refs[0].mcid).toEqual(0);
+      expect(structureContent.refs[1].pageRef.toString()).toEqual('13 0 R');
+      expect(structureContent.refs[1].mcid).toEqual(0);
       expect(docData).toContainChunk([
-        `9 0 obj`,
+        `11 0 obj`,
         `<<
 /Length ${stream.length}
 >>`,
@@ -120,6 +156,244 @@ EMC
         `endobj`
       ]);
     });
+  });
 
+  describe('structure tree', () => {
+    test('atomically constructed', () => {
+      const docData = logData(document);
+
+      const pContent1 = document.markStructureContent("P");
+      const linkContent = document.markStructureContent("Link");
+      const pContent2 = document.markStructureContent("P");
+      const pContent3 = document.markStructureContent("P");
+      document.markContent("Span");
+
+      const section1 = document.struct('Section', [
+        document.struct('P', [
+          pContent1,
+          document.struct('Link', [ linkContent ]),
+          pContent2
+        ])
+      ]);
+      const section2 = document.struct('Section', [
+        document.struct('P', [
+          pContent3,
+        ])
+      ]);
+      document.addStructure(section1).addStructure(section2);
+
+      document.end();
+
+      expect(docData).toContainChunk([
+        `3 0 obj`,
+        /\/StructTreeRoot 8 0 R/,
+        `endobj`
+      ]);
+      expect(docData).toContainChunk([
+        `3 0 obj`,
+        /\/Markings 9 0 R/,
+        `endobj`
+      ]);
+      expect(docData).toContainChunk([
+        `8 0 obj`,
+        `<<
+/Type /StructTreeRoot
+/ParentTree <<
+  /Nums [
+    0 [11 0 R 10 0 R 11 0 R 13 0 R]
+]
+>>
+/ParentTreeNextKey 1
+/K [12 0 R 14 0 R]
+>>`,
+        `endobj`
+      ]);
+      expect(docData).toContainChunk([
+        `12 0 obj`,
+        `<<
+/S /Section
+/K [11 0 R]
+/P 8 0 R
+>>`,
+        `endobj`
+      ]);
+      expect(docData).toContainChunk([
+        `11 0 obj`,
+        `<<
+/S /P
+/K [0 10 0 R 2]
+/Pg 7 0 R
+/P 12 0 R
+>>`,
+        `endobj`
+      ]);
+      expect(docData).toContainChunk([
+        `10 0 obj`,
+        `<<
+/S /Link
+/K [1]
+/Pg 7 0 R
+/P 11 0 R
+>>`,
+        `endobj`
+      ]);
+      expect(docData).toContainChunk([
+        `14 0 obj`,
+        `<<
+/S /Section
+/K [13 0 R]
+/P 8 0 R
+>>`,
+        `endobj`
+      ]);
+      expect(docData).toContainChunk([
+        `13 0 obj`,
+        `<<
+/S /P
+/K [3]
+/Pg 7 0 R
+/P 14 0 R
+>>`,
+        `endobj`
+      ]);
+      expect(docData).toContainChunk([
+        `9 0 obj`,
+        `<<
+>>`,
+        `endobj`
+      ]);
+    });
+
+    test('incrementally constructed', () => {
+      const docData = logData(document);
+
+      const pContent1 = document.markStructureContent("P");
+      const linkContent = document.markStructureContent("Link");
+      const pContent2 = document.markStructureContent("P");
+      const pContent3 = document.markStructureContent("P");
+      document.markContent("Span");
+
+      const section1 = document.struct('Section');
+      document.addStructure(section1);
+      const section2 = document.struct('Section');
+      document.addStructure(section2);
+      const link = document.struct('Link');
+      link.add(linkContent);
+      const p1 = document.struct('P');
+      section1.add(p1);
+      p1.add(pContent1).add(link).add(pContent2).end();
+      const p2 = document.struct('P');
+      p2.add(pContent3);
+      section2.add(p2);
+
+      document.end();
+
+      expect(docData).toContainChunk([
+        `3 0 obj`,
+        /\/StructTreeRoot 8 0 R/,
+        `endobj`
+      ]);
+      expect(docData).toContainChunk([
+        `3 0 obj`,
+        /\/Markings 9 0 R/,
+        `endobj`
+      ]);
+      expect(docData).toContainChunk([
+        `8 0 obj`,
+        `<<
+/Type /StructTreeRoot
+/ParentTree <<
+  /Nums [
+    0 [13 0 R 12 0 R 13 0 R 14 0 R]
+]
+>>
+/ParentTreeNextKey 1
+/K [10 0 R 11 0 R]
+>>`,
+        `endobj`
+      ]);
+      expect(docData).toContainChunk([
+        `10 0 obj`,
+        `<<
+/S /Section
+/P 8 0 R
+/K [13 0 R]
+>>`,
+        `endobj`
+      ]);
+      expect(docData).toContainChunk([
+        `13 0 obj`,
+        `<<
+/S /P
+/P 10 0 R
+/K [0 12 0 R 2]
+/Pg 7 0 R
+>>`,
+        `endobj`
+      ]);
+      expect(docData).toContainChunk([
+        `12 0 obj`,
+        `<<
+/S /Link
+/K [1]
+/Pg 7 0 R
+/P 13 0 R
+>>`,
+        `endobj`
+      ]);
+      expect(docData).toContainChunk([
+        `11 0 obj`,
+        `<<
+/S /Section
+/P 8 0 R
+/K [14 0 R]
+>>`,
+        `endobj`
+      ]);
+      expect(docData).toContainChunk([
+        `14 0 obj`,
+        `<<
+/S /P
+/K [3]
+/Pg 7 0 R
+/P 11 0 R
+>>`,
+        `endobj`
+      ]);
+      expect(docData).toContainChunk([
+        `9 0 obj`,
+        `<<
+>>`,
+        `endobj`
+      ]);
+    });
+
+    test('with options', () => {
+      const docData = logData(document);
+
+      document.addStructure(document.struct('P', {
+        title: "My Title",
+        lang: "en-AU",
+        alt: "My Alternative",
+        expanded: "My Expansion",
+        actual: "My Actual Text"
+      }));
+
+      document.end();
+
+      expect(docData).toContainChunk([
+        `8 0 obj`,
+        `<<
+/S /P
+/T (My Title)
+/Lang (en-AU)
+/Alt (My Alternative)
+/E (My Expansion)
+/ActualText (My Actual Text)
+/P 9 0 R
+>>`,
+        `endobj`
+      ]);
+    });
   });
 });
