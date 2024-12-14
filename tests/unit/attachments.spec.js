5a4327d19e61cbfcd5535daf3dd9c0640cde53c3
Libor M. <liborm85@gmail.com>
2024-12-14 12:48:36 +0100

    fixed tests

--- tests/unit/attachments.spec.js
+++ tests/unit/attachments.spec.js
@@ -54,6 +54,7 @@ describe('file', () => {
       `9 0 obj`,
       `<<
 /Type /Filespec
+/AFRelationship /Unspecified
 /F (file.txt)
 /EF <<
 /F 8 0 R
@@ -112,6 +113,7 @@ describe('file', () => {
       `9 0 obj`,
       `<<
 /Type /Filespec
+/AFRelationship /Unspecified
 /F (file.txt)
 /EF <<
 /F 8 0 R
