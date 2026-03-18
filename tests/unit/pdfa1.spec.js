2ed27231d0268ea50f6c9c3a9b86cbeb3bc75664
andreiaugustin <andreiaugustin98@gmail.com>
2026-03-18 08:17:33 +0200

    prettified

--- tests/unit/pdfa1.spec.js
+++ tests/unit/pdfa1.spec.js
@@ -123,7 +123,7 @@ describe('PDF/A-1', () => {
       '<pdf:Producer>Unit Test for PDFKit</pdf:Producer>',
     );
   });
-  
+
   test('CIDSet correctly identifies all glyphs in the subset', () => {
     let options = {
       autoFirstPage: false,
