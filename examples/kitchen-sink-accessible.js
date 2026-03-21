c8b9ab08bd6f5283289f5774332b6b96834ccd4b
Andrei Augustin <36695484+andreiaugustin@users.noreply.github.com>
2026-03-21 16:33:53 +0200

    make kitchen-sink-accessible example fully PDF/UA compliant (#1707)
    * make kitchen-sink-accessible example fully PDF/UA compliant
    
    * added unit test for structure element and updated docs
    
    * updated changelog

--- examples/kitchen-sink-accessible.js
+++ examples/kitchen-sink-accessible.js
@@ -39,6 +39,7 @@ struct.add(
 var imageSection = doc.struct('Sect');
 struct.add(imageSection);
 
+doc.outline.addItem('PNG and JPEG images:'); // add a bookmark for the image section's H1
 imageSection.add(
   doc.struct('H1', () => {
     doc.fontSize(18).text('PNG and JPEG images: ');
@@ -49,7 +50,8 @@ imageSection.add(
   doc.struct(
     'Figure',
     {
-      alt: 'Promotional image of an Apple laptop. '
+      alt: 'Promotional image of an Apple laptop. ',
+      bbox: [100, 160, 512, 387]
     },
     () => {
       doc.image('images/test.png', 100, 160, {
@@ -64,7 +66,8 @@ imageSection.add(
     'Figure',
     {
       alt:
-        'Photograph of a path flanked by blossoming trees with surrounding hedges. '
+        'Photograph of a path flanked by blossoming trees with surrounding hedges. ',
+      bbox: [190, 400, 415, 700]
     },
     () => {
       doc.image('images/test.jpeg', 190, 400, {
@@ -83,6 +86,7 @@ doc.addPage();
 var vectorSection = doc.struct('Sect');
 struct.add(vectorSection);
 
+doc.outline.addItem('Vector graphics:'); // add a bookmark for the vector graphics section's H1
 vectorSection.add(
   doc.struct('H1', () => {
     doc.fontSize(25).text('Here are some vector graphics... ', 100, 100);
@@ -93,15 +97,19 @@ vectorSection.add(
   doc.struct(
     'Figure',
     {
-      alt: 'Orange triangle. '
+      alt: 'Orange triangle. ',
+      bbox: [100, 150, 200, 250]
     },
     () => {
+      // we set fill color before path construction to comply with ISO 32000-2 Figure 9
       doc
         .save()
+        .fillColor('#FF8800')
         .moveTo(100, 150)
         .lineTo(100, 250)
         .lineTo(200, 250)
-        .fill('#FF8800');
+        .fill()
+        .restore();
     }
   )
 );
@@ -110,10 +118,12 @@ vectorSection.add(
   doc.struct(
     'Figure',
     {
-      alt: 'Purple circle. '
+      alt: 'Purple circle. ',
+      bbox: [230, 150, 330, 250]
     },
     () => {
-      doc.circle(280, 200, 50).fill('#7722FF');
+      // we set fill color before path construction to comply with ISO 32000-2 Figure 9
+      doc.save().fillColor('#7722FF').circle(280, 200, 50).fill().restore();
     }
   )
 );
@@ -122,16 +132,20 @@ vectorSection.add(
   doc.struct(
     'Figure',
     {
-      alt: 'Red star with hollow center. '
+      alt: 'Red star with hollow center. ',
+      bbox: [360, 128, 504, 266]
     },
     () => {
+      // we set fill color before path construction to comply with ISO 32000-2 Figure 9
       doc
+        .save()
+        .fillColor('red')
         .scale(0.6)
         .translate(470, 140)
         // render an SVG path
         .path('M 250,75 L 323,301 131,161 369,161 177,301 z')
         // fill using the even-odd winding rule
-        .fill('red', 'even-odd')
+        .fill('even-odd')
         .restore();
     }
   )
@@ -143,6 +157,7 @@ vectorSection.end();
 var wrappedSection = doc.struct('Sect');
 struct.add(wrappedSection);
 
+doc.outline.addItem('PNG and JPEG images:'); // add a bookmark for the wrapped text section's H1
 wrappedSection.add(
   doc.struct('H1', () => {
     doc
@@ -155,7 +170,7 @@ wrappedSection.add(
 
 var loremIpsum =
   'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam in suscipit purus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Vivamus nec hendrerit felis. Morbi aliquam facilisis risus eu lacinia. Sed eu leo in turpis fringilla hendrerit. Ut nec accumsan nisl. Suspendisse rhoncus nisl posuere tortor tempus et dapibus elit porta. Cras leo neque, elementum a rhoncus ut, vestibulum non nibh. Phasellus pretium justo turpis. Etiam vulputate, odio vitae tincidunt ultricies, eros odio dapibus nisi, ut tincidunt lacus arcu eu elit. Aenean velit erat, vehicula eget lacinia ut, dignissim non tellus. Aliquam nec lacus mi, sed vestibulum nunc. Suspendisse potenti. Curabitur vitae sem turpis. Vestibulum sed neque eget dolor dapibus porttitor at sit amet sem. Fusce a turpis lorem. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;\nMauris at ante tellus. Vestibulum a metus lectus. Praesent tempor purus a lacus blandit eget gravida ante hendrerit. Cras et eros metus. Sed commodo malesuada eros, vitae interdum augue semper quis. Fusce id magna nunc. Curabitur sollicitudin placerat semper. Cras et mi neque, a dignissim risus. Nulla venenatis porta lacus, vel rhoncus lectus tempor vitae. Duis sagittis venenatis rutrum. Curabitur tempor massa tortor.';
-doc.text(loremIpsum, {
+doc.font('Palatino').text(loremIpsum, {
   width: 412,
   align: 'justify',
   indent: 30,
@@ -172,6 +187,7 @@ doc.addPage();
 var tigerSection = doc.struct('Sect');
 struct.add(tigerSection);
 
+doc.outline.addItem('Tiger line art:'); // add a bookmark for the tiger section's H1
 tigerSection.add(
   doc.struct('H1', () => {
     doc
@@ -185,7 +201,8 @@ tigerSection.add(
   doc.struct(
     'Figure',
     {
-      alt: 'Tiger line art. '
+      alt: 'Tiger line art. ',
+      bbox: [30, 140, 540, 680]
     },
     () => {
       var i, len, part;
@@ -193,18 +210,25 @@ tigerSection.add(
       for (i = 0, len = tiger.length; i < len; i++) {
         part = tiger[i];
         doc.save();
-        doc.path(part.path); // render an SVG path
+        // we set fill color before path construction to comply with ISO 32000-2 Figure 9
+        if (part.fill !== 'none') {
+          doc.fillColor(part.fill);
+        }
+        if (part.stroke !== 'none') {
+          doc.strokeColor(part.stroke);
+        }
         if (part['stroke-width']) {
           doc.lineWidth(part['stroke-width']);
         }
+        doc.path(part.path); // render an SVG path
         if (part.fill !== 'none' && part.stroke !== 'none') {
-          doc.fillAndStroke(part.fill, part.stroke);
+          doc.fillAndStroke();
         } else {
           if (part.fill !== 'none') {
-            doc.fill(part.fill);
+            doc.fill();
           }
           if (part.stroke !== 'none') {
-            doc.stroke(part.stroke);
+            doc.stroke();
           }
         }
         doc.restore();
@@ -222,7 +246,10 @@ doc.addPage();
 var linkSection = doc.struct('Sect');
 struct.add(linkSection);
 
-linkSection.add(
+var linkParagraph = doc.struct('P');
+linkSection.add(linkParagraph);
+
+linkParagraph.add(
   doc.struct(
     'Link',
     {
@@ -237,6 +264,7 @@ linkSection.add(
   )
 );
 
+linkParagraph.end();
 linkSection.end();
 
 // Add a list with a font loaded from a TrueType collection file
