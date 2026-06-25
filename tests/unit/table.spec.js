4fdaf60ca3a8a6bb0a08ad8535e75014487d5e2e
ykw9263 <180926524+ykw9263@users.noreply.github.com>
2026-06-26 00:07:37 +0800

    Add unit tests for new deep merge behaviour

--- tests/unit/table.spec.js
+++ tests/unit/table.spec.js
@@ -1,6 +1,7 @@
 import PDFDocument from '../../lib/document';
 import PDFTable from '../../lib/table';
 import { deepMerge } from '../../lib/table/utils';
+import fs from 'fs';
 
 describe('table', () => {
   test('created', () => {
@@ -13,6 +14,124 @@ describe('table', () => {
     const table = document.table();
     table.row(['A', 'B', 'C']);
     expect(table._columnWidths.length).toBe(3);
+  });
+
+  describe('font', () => {
+    test('colum standard font', () => {
+      const document = new PDFDocument();
+      const table = document.table({
+        columnStyles: {
+          font: { src: 'Courier' },
+        },
+      });
+      table.row(['A']);
+      expect(table._columnWidths.length).toBe(1);
+    });
+
+    test('colum embeded font with path', () => {
+      const document = new PDFDocument();
+      const table = document.table({
+        columnStyles: {
+          font: { src: 'tests/fonts/Roboto-Regular.ttf' },
+        },
+      });
+      table.row(['A']);
+      expect(table._columnWidths.length).toBe(1);
+    });
+
+    test('colum embeded font with Buffer', () => {
+      const document = new PDFDocument();
+      const buffer = fs.readFileSync('tests/fonts/Roboto-Regular.ttf');
+      console.log('buffer instanceof Buffer:', buffer instanceof Buffer);
+      const table = document.table({
+        columnStyles: {
+          font: {
+            src: buffer,
+          },
+        },
+      });
+      table.row(['A']);
+      expect(table._columnWidths.length).toBe(1);
+    });
+
+    test('row standard font', () => {
+      const document = new PDFDocument();
+      const table = document.table({
+        rowStyles: {
+          font: { src: 'Courier' },
+        },
+      });
+      table.row(['A']);
+      expect(table._columnWidths.length).toBe(1);
+    });
+
+    test('row embeded font with path', () => {
+      const document = new PDFDocument();
+      const table = document.table({
+        rowStyles: {
+          font: { src: 'tests/fonts/Roboto-Regular.ttf' },
+        },
+      });
+      table.row(['A']);
+      expect(table._columnWidths.length).toBe(1);
+    });
+
+    test('row embeded font with Buffer', () => {
+      const document = new PDFDocument();
+      const buffer = fs.readFileSync('tests/fonts/Roboto-Regular.ttf');
+      console.log('buffer instanceof Buffer:', buffer instanceof Buffer);
+      const table = document.table({
+        rowStyles: {
+          font: {
+            src: buffer,
+          },
+        },
+      });
+      table.row(['A']);
+      expect(table._columnWidths.length).toBe(1);
+    });
+
+    test('cell standard font', () => {
+      const document = new PDFDocument();
+      const table = document.table();
+      table.row([
+        {
+          text: 'A',
+          font: {
+            src: 'Courier',
+          },
+        },
+      ]);
+      expect(table._columnWidths.length).toBe(1);
+    });
+
+    test('cell embeded font with path', () => {
+      const document = new PDFDocument();
+      const table = document.table();
+      table.row([
+        {
+          text: 'A',
+          font: {
+            src: 'tests/fonts/Roboto-Regular.ttf',
+          },
+        },
+      ]);
+      expect(table._columnWidths.length).toBe(1);
+    });
+
+    test('cell embeded font with Buffer', () => {
+      const document = new PDFDocument();
+      const table = document.table();
+      table.row([
+        {
+          text: 'A',
+          font: {
+            src: fs.readFileSync('tests/fonts/Roboto-Regular.ttf'),
+          },
+        },
+      ]);
+      expect(table._columnWidths.length).toBe(1);
+    });
   });
 });
 
@@ -28,6 +147,16 @@ describe('utils', () => {
       [1, {}, 1],
       [{ a: 'hello' }, { a: {} }, { a: 'hello' }],
       [{ a: { b: 'hello' } }, { a: { b: 'world' } }, { a: { b: 'world' } }],
+      [
+        { a: Buffer.from([1, 2, 3]) },
+        { b: Buffer.from([4, 5, 6]) },
+        { a: Buffer.from([1, 2, 3]), b: Buffer.from([4, 5, 6]) },
+      ],
+      [
+        { a: new Uint8Array([1, 2, 3]) },
+        { b: new Uint8Array([4, 5, 6]) },
+        { a: new Uint8Array([1, 2, 3]), b: new Uint8Array([4, 5, 6]) },
+      ],
     ])('%o -> %o', function () {
       const opts = Array.from(arguments);
       const expected = opts.splice(-1, 1)[0];
