f5ccc929c10df74c4695c502f997cf625ec9d0a2
ykw9263 <ykw9263@proton.me>
2026-06-26 16:42:36 +0800

    Rewrite table font unit test

--- tests/unit/table.spec.js
+++ tests/unit/table.spec.js
@@ -1,3 +1,4 @@
+import { vi } from 'vitest';
 import PDFDocument from '../../lib/document';
 import PDFTable from '../../lib/table';
 import { deepMerge } from '../../lib/table/utils';
@@ -17,120 +18,193 @@ describe('table', () => {
   });
 
   describe('font', () => {
-    test('column standard font', () => {
+    test('column font', () => {
+      const standardFont = 'Courier';
+      const fontPath = 'tests/fonts/Roboto-Regular.ttf';
+      const fontBuffer = fs.readFileSync('tests/fonts/Roboto-Regular.ttf');
       const document = new PDFDocument();
+      const fontSpy = vi.spyOn(document, 'font');
+
       const table = document.table({
-        columnStyles: {
-          font: { src: 'Courier' },
-        },
+        columnStyles: [
+          { font: { src: standardFont } },
+          { font: { src: fontPath } },
+          { font: { src: fontBuffer } },
+        ],
       });
-      table.row(['A']);
-      expect(table._columnWidths.length).toBe(1);
+      table.row(['A', 'B', 'C']);
+      expect(fontSpy).toHaveBeenCalledWith(
+        standardFont,
+        expect.toSatisfy(() => true),
+      );
+      expect(fontSpy).toHaveBeenCalledWith(
+        fontPath,
+        expect.toSatisfy(() => true),
+      );
+      expect(fontSpy).toHaveBeenCalledWith(
+        fontBuffer,
+        expect.toSatisfy(() => true),
+      );
     });
 
-    test('column embeded font with path', () => {
+    test('row font', () => {
+      const standardFont = 'Courier';
+      const fontPath = 'tests/fonts/Roboto-Regular.ttf';
+      const fontBuffer = fs.readFileSync('tests/fonts/Roboto-Regular.ttf');
       const document = new PDFDocument();
+      const fontSpy = vi.spyOn(document, 'font');
+
       const table = document.table({
-        columnStyles: {
-          font: { src: 'tests/fonts/Roboto-Regular.ttf' },
-        },
+        rowStyles: [
+          { font: { src: standardFont } },
+          { font: { src: fontPath } },
+          { font: { src: fontBuffer } },
+        ],
       });
       table.row(['A']);
-      expect(table._columnWidths.length).toBe(1);
+      table.row(['B']);
+      table.row(['C']);
+      expect(fontSpy).toHaveBeenCalledWith(
+        standardFont,
+        expect.toSatisfy(() => true),
+      );
+      expect(fontSpy).toHaveBeenCalledWith(
+        fontPath,
+        expect.toSatisfy(() => true),
+      );
+      expect(fontSpy).toHaveBeenCalledWith(
+        fontBuffer,
+        expect.toSatisfy(() => true),
+      );
     });
 
-    test('column embeded font with Buffer', () => {
+    test('cell font', () => {
+      const standardFont = 'Courier';
+      const fontPath = 'tests/fonts/Roboto-Regular.ttf';
+      const fontBuffer = fs.readFileSync('tests/fonts/Roboto-Regular.ttf');
       const document = new PDFDocument();
-      const buffer = fs.readFileSync('tests/fonts/Roboto-Regular.ttf');
-      console.log('buffer instanceof Buffer:', buffer instanceof Buffer);
-      const table = document.table({
-        columnStyles: {
-          font: {
-            src: buffer,
-          },
-        },
-      });
-      table.row(['A']);
-      expect(table._columnWidths.length).toBe(1);
-    });
+      const fontSpy = vi.spyOn(document, 'font');
 
-    test('row standard font', () => {
-      const document = new PDFDocument();
-      const table = document.table({
-        rowStyles: {
-          font: { src: 'Courier' },
-        },
-      });
-      table.row(['A']);
-      expect(table._columnWidths.length).toBe(1);
-    });
-
-    test('row embeded font with path', () => {
-      const document = new PDFDocument();
-      const table = document.table({
-        rowStyles: {
-          font: { src: 'tests/fonts/Roboto-Regular.ttf' },
-        },
-      });
-      table.row(['A']);
-      expect(table._columnWidths.length).toBe(1);
-    });
-
-    test('row embeded font with Buffer', () => {
-      const document = new PDFDocument();
-      const buffer = fs.readFileSync('tests/fonts/Roboto-Regular.ttf');
-      console.log('buffer instanceof Buffer:', buffer instanceof Buffer);
-      const table = document.table({
-        rowStyles: {
-          font: {
-            src: buffer,
-          },
-        },
-      });
-      table.row(['A']);
-      expect(table._columnWidths.length).toBe(1);
-    });
-
-    test('cell standard font', () => {
-      const document = new PDFDocument();
       const table = document.table();
+      table.row([
+        { text: 'A', font: { src: standardFont } },
+        { text: 'B', font: { src: fontPath } },
+        { text: 'C', font: { src: fontBuffer } },
+      ]);
+      expect(fontSpy).toHaveBeenCalledWith(
+        standardFont,
+        expect.toSatisfy(() => true),
+      );
+      expect(fontSpy).toHaveBeenCalledWith(
+        fontPath,
+        expect.toSatisfy(() => true),
+      );
+      expect(fontSpy).toHaveBeenCalledWith(
+        fontBuffer,
+        expect.toSatisfy(() => true),
+      );
+    });
+
+    test('merge table font', () => {
+      const colFamily = 'family1';
+      const rowFamily = 'family2';
+      const cellFamily = 'family3';
+      const fontSrcs = {
+        colStandardFont: 'Courier',
+        colFontPath: 'tests/fonts/Roboto-Regular.ttf',
+        colFontBuffer: fs.readFileSync('tests/fonts/Roboto-Regular.ttf'),
+        rowStandardFont: 'Courier-Bold',
+        rowFontPath: 'tests/fonts/Roboto-Medium.ttf',
+        rowFontBuffer: fs.readFileSync('tests/fonts/Roboto-Medium.ttf'),
+        cellStandardFont: 'Courier-Oblique',
+        cellFontPath: 'tests/fonts/Roboto-MediumItalic.ttf',
+        cellFontBuffer: fs.readFileSync('tests/fonts/Roboto-MediumItalic.ttf'),
+      };
+      const fontSrcSet = Object.values(fontSrcs);
+
+      /**
+       * Check whether given spy has been called with specified allowed fonts
+       * and not other fonts within concerned font set
+       * @param {*} fontSpy
+       * @param {import('../../lib/table/utils').Font[]} allowedFonts
+       */
+      function expectFonts(
+        fontSpy,
+        allowedFonts = [],
+        testedFonts = fontSrcSet,
+      ) {
+        const allowedFontSrc = allowedFonts.map((font) => {
+          expect(fontSpy).toHaveBeenCalledWith(font.src, font.family);
+          return font.src;
+        });
+        testedFonts.forEach((fontSrc) => {
+          if (!allowedFontSrc.includes(fontSrc)) {
+            expect(fontSpy).not.toHaveBeenCalledWith(
+              fontSrc,
+              expect.toSatisfy(() => true),
+            );
+          }
+        });
+      }
+      const document = new PDFDocument();
+      const fontSpy = vi.spyOn(document, 'font');
+
+      const table = document.table({
+        columnStyles: [
+          { font: { src: fontSrcs.colStandardFont, family: colFamily } },
+          { font: { src: fontSrcs.colFontPath, family: colFamily } },
+          { font: { src: fontSrcs.colFontBuffer, family: colFamily } },
+        ],
+        rowStyles: [
+          {},
+          { font: { src: fontSrcs.rowStandardFont, family: rowFamily } },
+          { font: { src: fontSrcs.rowFontPath, family: rowFamily } },
+          { font: { src: fontSrcs.rowFontBuffer, family: rowFamily } },
+          { font: { src: fontSrcs.rowFontBuffer, family: rowFamily } },
+        ],
+      });
+      // fonts in column styles
+      fontSpy.mockClear();
+      table.row([{ text: 'A' }, { text: 'B' }, { text: 'C' }]);
+      expectFonts(fontSpy, [
+        { src: fontSrcs.colStandardFont, family: colFamily },
+        { src: fontSrcs.colFontPath, family: colFamily },
+        { src: fontSrcs.colFontBuffer, family: colFamily },
+      ]);
+
+      // fonts in column + row styles
+      fontSpy.mockClear();
+      table.row([{ text: 'A' }, { text: 'B' }, { text: 'C' }]);
+      expectFonts(fontSpy, [
+        { src: fontSrcs.rowStandardFont, family: rowFamily },
+      ]);
+      fontSpy.mockClear();
+      table.row([{ text: 'A' }, { text: 'B' }, { text: 'C' }]);
+      expectFonts(fontSpy, [{ src: fontSrcs.rowFontPath, family: rowFamily }]);
+      fontSpy.mockClear();
+      table.row([{ text: 'A' }, { text: 'B' }, { text: 'C' }]);
+      expectFonts(fontSpy, [
+        { src: fontSrcs.rowFontBuffer, family: rowFamily },
+      ]);
+
+      // fonts in column + row + cell style
+      fontSpy.mockClear();
       table.row([
         {
           text: 'A',
-          font: {
-            src: 'Courier',
-          },
+          font: { src: fontSrcs.cellStandardFont, family: cellFamily },
         },
-      ]);
-      expect(table._columnWidths.length).toBe(1);
-    });
-
-    test('cell embeded font with path', () => {
-      const document = new PDFDocument();
-      const table = document.table();
-      table.row([
+        { text: 'B', font: { src: fontSrcs.cellFontPath, family: cellFamily } },
         {
-          text: 'A',
-          font: {
-            src: 'tests/fonts/Roboto-Regular.ttf',
-          },
+          text: 'C',
+          font: { src: fontSrcs.cellFontBuffer, family: cellFamily },
         },
       ]);
-      expect(table._columnWidths.length).toBe(1);
-    });
-
-    test('cell embeded font with Buffer', () => {
-      const document = new PDFDocument();
-      const table = document.table();
-      table.row([
-        {
-          text: 'A',
-          font: {
-            src: fs.readFileSync('tests/fonts/Roboto-Regular.ttf'),
-          },
-        },
+      expectFonts(fontSpy, [
+        { src: fontSrcs.cellStandardFont, family: cellFamily },
+        { src: fontSrcs.cellFontPath, family: cellFamily },
+        { src: fontSrcs.cellFontBuffer, family: cellFamily },
       ]);
-      expect(table._columnWidths.length).toBe(1);
     });
   });
 });
